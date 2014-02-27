b56aafdf11c8aee1f644cbe9bd1c9543459cd2f6
pablok <Pablo.Karlsson@gmail.com>
2014-02-27 07:30:21 +0100

    basic ui being implemented

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,24 @@
 function QuickWindowCtrl($scope) {
-
+  log.info("Initializing quick-window");
+  
+  $scope.$watch('today', function() {
+    $scope.date = $scope.today.toString("MM-dd");
+    $scope.year = $scope.today.toString("yyyy");
+    
+  }, true);
+  
+  $scope.today = Date.today();
+  
+  $scope.nextDay = function() {
+    $scope.today = $scope.today.next().day();
+  };
+  
+  $scope.prevDay = function() {
+    $scope.today = $scope.today.prev().day();
+  };
+  
 };
-function SettingsCtrl($scope) {
 
+function SettingsCtrl($scope) {
+  log.info("Initializing settings");
 };
